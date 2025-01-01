597559be337805b4dfdda71ac57386fece29da24
nanchaos <weiwenjie-khd@bestpay.com.cn>
2025-01-01 11:12:21 +0800

    change

--- src/main/java/com/nanchaos/tech/util/PrintSysInfo.java
+++ src/main/java/com/nanchaos/tech/util/PrintSysInfo.java
@@ -13,9 +13,5 @@ public interface PrintSysInfo {
 
     String getSysInfo();
 
-    default String sysInfo() {
-        return getClass().getName() + "@" + Integer.toHexString(hashCode());
-    }
-
     int getLogHashCode();
 }
